313e19cdcdc556a0e05169e8220b93ae7e5d70b2
Zion Miller <djzionmiller@gmail.com>
2022-07-06 12:37:42 -0700

    commit tree 44d0cde72f53c4e04f690aa14205c6b5721d7f53

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+// ✓ returns a year the Denver Broncos won the superbowl
+// ✓ returns undefined if the record has no win objects
+
+function superbowlWin(record){
+    const wins = record.find(record => record.result === 'W');
+    console.log (wins);
+    if (wins){
+        return (`${wins.year}`);
+    }
+}
